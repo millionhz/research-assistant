24009569ca3b9b4a7626e2f49a978208fac4a8bf
Muhammad Hamza <m.waqas.hamza@gmail.com>
2024-04-24 16:49:54 +0500

    add clear chat

--- chatui.py
+++ chatui.py
@@ -2,8 +2,12 @@ import streamlit as st
 from canopy.models.data_models import UserMessage, AssistantMessage
 from chatpdf import chat
 
+
 def llm_chat():
     st.title("Chat with Research")
+
+    if st.button("Clear Conversation"):
+        st.session_state.messages = []
 
     if "messages" not in st.session_state:
         st.session_state.messages = []
@@ -17,13 +21,13 @@ def llm_chat():
         with st.chat_message("user"):
             st.markdown(prompt)
 
-        assistant_response, updated_history = chat(prompt, st.session_state.messages)
+        assistant_response, updated_history = chat(
+            prompt, st.session_state.messages)
         st.session_state.messages = updated_history
         with st.chat_message("assistant"):
             st.markdown(assistant_response)
 
+
 if __name__ == "__main__":
     st.set_page_config(page_title="LLM Chat", layout="wide")
     llm_chat()
-
-
